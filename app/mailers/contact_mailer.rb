bc93ba3a47c776586beec95c4df3ec92be1057b5
Harshin Kanabar <harshin.kanabar@gmail.com>
2020-01-23 22:15:24 +0000

    Created contact form mailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,12 @@
+class ContactMailer < ActionMailer::Base
+    default to: 'harshinkanabar@yahoo.com'
+    
+    def contact_email(name,email,comments)
+        @name= name
+        @email = email
+        @comments = comments
+
+mail(from: email,subject:'Contact Form Message')    
+    end
+        
+end 
